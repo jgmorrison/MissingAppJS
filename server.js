6bf43df8eaae55696f02376c230ef63cd5ff7ac4
jgmorrison <jmorrison@continuum.io>
2017-03-13 13:34:06 -0500

    Added express.static for /src/ directory to server static files.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/src/'));
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/src/html/index.html');
 });
