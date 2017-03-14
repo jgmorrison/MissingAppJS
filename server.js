c2d48a572ece2bdfa0f6bb4e4581c329a8733336
jgmorrison <jmorrison@continuum.io>
2017-03-14 15:30:34 -0500

    Created ng-repeat div for cases in result window.

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/missing_persons');
+mongoose.connection.on("open", function () {
+	console.log("mongodb is connected!!");
+});
 
 var app = express();
 
 app.use(express.static(__dirname + '/src/'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/src/html/index.html');
 });
+
+app.get("/state", function (req, res) {
+
+    charley.find(function (err, data) {
+	    res.send(data);
+    });
+});
+
+var Schema = mongoose.Schema;
+    
+    var charleySchema = new Schema({
+        _id: 0,
+        City: String,
+        Name: String,
+        DOB: String,
+        Gender: String,
+        Missing_Since: String,
+        State: String,
+        Height_Weight: String,
+        Classification: String,
+        Characteristics: String,
+        Link_URL: String,
+        Items: String,
+        Medical_Conditions: String,
+        Age: String
+    });
+
+var charley = mongoose.model("Charley", charleySchema, 'charley');
 
 app.listen('3000');
--- src/angular/app.js
+++ src/angular/app.js
@@ -1,8 +1,9 @@
 var app = angular.module("MyApp", []);
-app.controller("MyCtrl", function ($scope) {
+app.controller("MyCtrl", function ($scope, $http) {
 	$scope.greeting = "Hello App!";
 	$scope.selectedState = "";
 	$scope.cityList = [];
+	$scope.results = {};
 
 	$scope.updateCityList = function () {
 		$scope.cityList = $scope.cityStateInfo[$scope.selectedState];
@@ -12,5 +13,12 @@ app.controller("MyCtrl", function ($scope) {
 		'California' : ['Citrus Heights', 'Sacramento'],
 		'Nevada' : ['Carson City', 'Reno'],
 		'Texas' : ['Austin', 'Dallas']
+	};
+
+	$scope.getResults = function () {
+		$http.get('http://localhost:3000/state')
+			.then(function (res) {
+				$scope.results = res.data;
+			}); 
 	};
 });
