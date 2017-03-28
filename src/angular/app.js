ac40d1eae1678d83ed7e12fbc4972d72e54ad749
jgmorrison <jmorrison@continuum.io>
2017-03-28 15:27:19 -0500

    Added ability to get data by state or state & city.

--- src/angular/app.js
+++ src/angular/app.js
@@ -1,12 +1,15 @@
 var app = angular.module("MyApp", []);
 app.controller("MyCtrl", function ($scope, $http) {
 	$scope.selectedStateIndex = "";
+    $scope.selectedState = "";
+    $scope.selectedCity = "";
 	$scope.cityList = [];
     $scope.stateList = [];
 	$scope.results = {};
 
 	$scope.updateCityList = function () {
 		$scope.cityList = $scope.cityStateInfo[$scope.selectedStateIndex].cities;
+        $scope.selectedState = $scope.cityStateInfo[$scope.selectedStateIndex].state;
 	};
 
 	$scope.cityStateInfo = {};
@@ -19,7 +22,7 @@ app.controller("MyCtrl", function ($scope, $http) {
 	$scope.getCityStateInfo();
 
 	$scope.getResults = function () {
-		$http.get('http://localhost:3000/data')
+		$http.get('http://localhost:3000/data/' + $scope.selectedState + '/' + $scope.selectedCity)
 			.then(function (res) {
 				$scope.results = res.data;
 			}); 
