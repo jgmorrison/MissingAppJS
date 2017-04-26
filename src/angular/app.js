e8238a55da1ffb579cce553dc6a408a06e9d8ca4
jgmorrison <jmorrison@continuum.io>
2017-04-26 12:58:13 -0500

    hide More Results button at end of results

--- src/angular/app.js
+++ src/angular/app.js
@@ -5,7 +5,7 @@ app.controller("MyCtrl", function ($scope, $http) {
     $scope.selectedCity = "";
 	$scope.cityList = [];
     $scope.stateList = [];
-	$scope.results = [];
+	$scope.results;
     $scope.selectedGender = "";
     $scope.selectedMonth = "";
     $scope.selectedDay = "";
@@ -20,7 +20,7 @@ app.controller("MyCtrl", function ($scope, $http) {
         $scope.selectedCity = "";
         $scope.cityList = [];
         $scope.stateList = [];
-        $scope.results = [];
+        $scope.results;
         $scope.selectedGender = "";
         $scope.selectedMonth = "";
         $scope.selectedDay = "";
@@ -72,9 +72,16 @@ app.controller("MyCtrl", function ($scope, $http) {
                 "resultLimit" : $scope.resultLimit}
             })
 			.then(function (res) {
-				$scope.results = $scope.results.concat(res.data);
-                $scope.skipAmount += 25;
-                $scope.isMoreResults = true;
+                if ($scope.results == undefined) {
+                    $scope.results = [];
+                }
+                if (res.data.length == 0) {
+                    $scope.isMoreResults = false;
+                } else {
+				    $scope.results = $scope.results.concat(res.data);
+                    $scope.skipAmount += 25;
+                    $scope.isMoreResults = true;
+                }
 			}); 
 	};
     
